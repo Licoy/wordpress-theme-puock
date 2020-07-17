f979bbb616b3cd4af31f0c3ae520868e6de878ba
望梅不止渴 <licoycn@gmail.com>
2020-07-17 17:25:10 +0800

    增加给外链加上nofollow及新窗口打开

--- fun-custom.php
+++ fun-custom.php
@@ -1,5 +1,49 @@
 <?php
 /**
- * 您的自定义主题函数部分
+ * 其他外部引用函数方法
  * ==================
  */
+
+/**
+ * 给外链加上nofollow及新窗口打开
+ * from: https://cn.wordpress.org/plugins/nofollow-for-external-link/
+ * license: GPL-2.0
+ */
+add_filter( 'the_content', 'cn_nf_url_parse');
+function cn_nf_url_parse( $content ) {
+    $regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>";
+    if(preg_match_all("/$regexp/siU", $content, $matches, PREG_SET_ORDER)) {
+        if( !empty($matches) ) {
+
+            $srcUrl = get_option('siteurl');
+            for ($i=0; $i < count($matches); $i++)
+            {
+
+                $tag = $matches[$i][0];
+                $tag2 = $matches[$i][0];
+                $url = $matches[$i][0];
+
+                $noFollow = '';
+
+                $pattern = '/target\s*=\s*"\s*_blank\s*"/';
+                preg_match($pattern, $tag2, $match, PREG_OFFSET_CAPTURE);
+                if( count($match) < 1 )
+                    $noFollow .= ' target="_blank" ';
+
+                $pattern = '/rel\s*=\s*"\s*[n|d]ofollow\s*"/';
+                preg_match($pattern, $tag2, $match, PREG_OFFSET_CAPTURE);
+                if( count($match) < 1 )
+                    $noFollow .= ' rel="nofollow" ';
+
+                $pos = strpos($url,$srcUrl);
+                if ($pos === false) {
+                    $tag = rtrim ($tag,'>');
+                    $tag .= $noFollow.'>';
+                    $content = str_replace($tag2,$tag,$content);
+                }
+            }
+        }
+    }
+    $content = str_replace(']]>', ']]>', $content);
+    return $content;
+}
