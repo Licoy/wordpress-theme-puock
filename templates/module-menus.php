e3c44e07b8a89d79a3fe19b1b875e93823d0671a
Licoy <licoycn@gmail.com>
2021-02-01 18:42:35 +0800

    :zap: 修复关于文章目录的显示问题

--- templates/module-menus.php
+++ templates/module-menus.php
@@ -4,10 +4,12 @@
  */
 ?>
 
-<div id="post-menus">
-    <div id="post-menu-state" title="打开或关闭文章目录">
-        <i class="puock-text ta3 czs-menu-l"></i>
+<?php if(is_single()):?>
+    <div id="post-menus">
+        <div id="post-menu-state" title="打开或关闭文章目录">
+            <i class="puock-text ta3 czs-menu-l"></i>
+        </div>
+        <div id="post-menu-content" class="animated slideInRight">
+        </div>
     </div>
-    <div id="post-menu-content" class="animated slideInRight">
-    </div>
-</div>
+<?php endif; ?>
