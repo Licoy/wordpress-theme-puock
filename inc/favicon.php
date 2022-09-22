7cca8a052bf281fdfaa0cdf8cae44f04dac869fe
Licoy <licoycn@gmail.com>
2022-09-23 07:39:01 +0800

    :sparkles: 增加对已有的icon缓存文件进行301重定向访问

--- inc/favicon.php
+++ inc/favicon.php
@@ -2,10 +2,11 @@
 
 function pk_get_website_favicon_ico($url, $cache_time, $default_ico, $basename = "favicon.ico")
 {
-    $cache_file = dirname(__FILE__) . '/../cache/' . md5($url) . '.ico';
+    $cache_filename = 'icon-'.md5($url) . '.ico';
+    $cache_file = dirname(__FILE__) . '/../cache/' . $cache_filename;
     if (is_file($cache_file)) {
         if (time() - filemtime($cache_file) <= $cache_time) {
-            pk_favicon_get_ico_contents($cache_file, $default_ico);
+            pk_favicon_get_ico_contents($cache_file, $cache_filename);
             return;
         }
     }
@@ -23,26 +24,36 @@ function pk_get_website_favicon_ico($url, $cache_time, $default_ico, $basename =
     if (curl_getinfo($ch, CURLINFO_HTTP_CODE) != 200) {
         curl_close($ch);
         @unlink($cache_file);
-        pk_favicon_put_default_and_output($cache_file, $default_ico);
+        pk_favicon_put_default_and_output($cache_file, $cache_filename, $default_ico);
         return;
     }
     $mimeArray = explode('/', curl_getinfo($ch, CURLINFO_CONTENT_TYPE));
     curl_close($ch);
     if (count($mimeArray) == 0 || $mimeArray[0] != 'image') {
         @unlink($cache_file);
-        pk_favicon_put_default_and_output($cache_file, $default_ico);
+        pk_favicon_put_default_and_output($cache_file, $cache_filename, $default_ico);
         return;
     }
-    pk_favicon_get_ico_contents($cache_file, $default_ico);
+    pk_favicon_get_ico_contents($cache_file, $cache_filename);
 }
 
-function pk_favicon_get_ico_contents($cache_file, $default_ico)
+function pk_favicon_get_ico_contents($cache_file, $cache_filename)
 {
     if (pk_favicon_validate($cache_file)) {
-        pk_favicon_output(file_get_contents($cache_file));
+        pk_favicon_http_redirect(301, 'cache/' . $cache_filename);
         return;
     }
-    pk_favicon_output(file_get_contents($default_ico));
+    pk_favicon_http_redirect(302, 'assets/img/favicon.ico');
+}
+
+function pk_favicon_http_redirect($code, $cache_filename)
+{
+    $url = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST']
+        . str_replace('inc/favicon.php', '', $_SERVER['SCRIPT_NAME']) . $cache_filename;
+    if ($code == 301) {
+        Header("HTTP/1.1 301 Moved Permanently");
+    }
+    Header("Location: " . $url);
 }
 
 function pk_favicon_validate($cache_file)
@@ -50,22 +61,13 @@ function pk_favicon_validate($cache_file)
     return @exif_imagetype($cache_file);
 }
 
-function pk_favicon_put_default_and_output($cache_file, $default_ico)
+function pk_favicon_put_default_and_output($cache_file, $cache_filename, $default_ico)
 {
     $data = file_get_contents($default_ico);
     $f = fopen($cache_file, 'w');
     fwrite($f, $data);
     fclose($f);
-    pk_favicon_output($data);
-}
-
-function pk_favicon_output($data)
-{
-    header('Cache-Control: public, max-age=0, must-revalidate');
-    header('Cache-Control: post-check=0, pre-check=0', false);
-    header('Pragma: no-cache');
-    header("content-type: image/png");
-    echo $data;
+    pk_favicon_http_redirect(301, 'cache/' . $cache_filename);
 }
 
 $url = @$_GET['url'];
