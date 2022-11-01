30f33ca4e9977bec4adb899ec0f8472d92229d42
Licoy <licoycn@gmail.com>
2022-11-01 16:34:35 +0800

    :sparkles: 优化点赞文字显示

--- templates/post-actions.php
+++ templates/post-actions.php
@@ -3,7 +3,7 @@
         <div class="d-flex justify-content-center w-100 c-sub">
             <div class="circle-button puock-bg text-center <?php if(puock_post_is_like()): ?>bg-primary text-light<?php endif; ?>"
                  id="post-like" data-id="<?php the_ID() ?>">
-                <i class="fa-regular fa-thumbs-up t-md"></i><span class="t-sm">&nbsp;<?php echo get_post_meta($post->ID,'puock_like',true) ?></span></div>
+                <i class="fa-regular fa-thumbs-up t-md"></i>&nbsp;<span class="t-sm"><?php echo get_post_meta($post->ID,'puock_like',true) ?></span></div>
             <?php if(pk_is_checked('post_reward')): ?>
                 <div class="circle-button puock-bg text-center" data-toggle="modal" data-target="#rewardModal"><span>赏</span></div>
             <?php endif; ?>
