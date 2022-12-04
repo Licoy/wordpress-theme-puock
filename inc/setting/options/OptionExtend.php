954aaf6a11b8ef5b7ffb8f68852fb7124e44cb0e
Licoy <licoycn@gmail.com>
2022-12-04 16:17:03 +0800

    :sparkles: 优化配置框架及选项

--- inc/setting/options/OptionExtend.php
+++ inc/setting/options/OptionExtend.php
@@ -16,7 +16,7 @@ class OptionExtend extends BaseOptionItem
                     'id' => 'office_mp_support',
                     'label' => 'Puock官方小程序支持',
                     'type' => 'switch',
-                    'sdt' => defined('PUOCK_MP_VERSION'),
+                    'value' => defined('PUOCK_MP_VERSION'),
                     'tips' => "Puock官方小程序支持，此选项安装小程序插件后会自动开启，如需关闭请在小程序插件中关闭",
                     'disabled' => true,
                 ],
