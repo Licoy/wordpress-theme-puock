5213d3465b0474a762749453cc5d59d856164d1e
Licoy <licoycn@gmail.com>
2023-02-05 17:24:04 +0800

    :lipstick: 重新设计时光圈列表样式

--- pages/template-moments.php
+++ pages/template-moments.php
@@ -14,7 +14,7 @@ get_header();
     $page = get_query_var('paged') ? get_query_var('paged') : 1;
     $cache_key = sprintf(PKC_MOMENTS, $page);
     $posts = pk_cache_get($cache_key);
-    if(!$posts){
+    if (!$posts) {
         $posts = query_posts(array(
             'post_type' => 'moments',
             'posts_per_page' => 10,
@@ -29,23 +29,27 @@ get_header();
                  class="col-lg-<?php pk_hide_sidebar_out('12', '8') ?> col-md-12 <?php pk_open_box_animated('animated fadeInLeft') ?> ">
                 <?php foreach ($posts as $post):setup_postdata($post);
                     the_post(); ?>
-                    <div class="mt20 p-block puock-text moments-item">
-                        <div class="meta p-flex-sc">
-                            <div class="avatar mr10">
-                                <img <?php echo pk_get_lazy_img_info(get_avatar_url(get_the_author_meta('email')),'md-avatar mt-1') ?>
-                                     alt="<?php the_author_meta('display_name') ?>"
-                                     title="<?php the_author_meta('display_name') ?>">
+                    <div class="mb20 puock-text moments-item">
+                        <div class="row">
+                            <div class="col-12 col-md-1">
+                                <a class="meta ta3" href="<?php echo get_author_posts_url($post->post_author) ?>">
+                                    <div class="avatar mb10">
+                                        <img <?php echo pk_get_lazy_img_info(get_avatar_url(get_the_author_meta('email')), 'md-avatar mt-1') ?>
+                                                alt="<?php the_author_meta('display_name') ?>"
+                                                title="<?php the_author_meta('display_name') ?>">
+                                    </div>
+                                    <div class="t-line-1 info fs12"><?php the_author_meta('display_name') ?></div>
+                                </a>
                             </div>
-                            <div class="info fs12">
-                                <div><?php the_author_meta('display_name') ?></div>
-                                <div class="c-sub"><?php echo get_the_date('Y-m-d'); ?></div>
-                            </div>
-                        </div>
-                        <div class="mt10 moment-content <?php get_entry_content_class() ?>">
-                            <?php the_content(); ?>
-                        </div>
-                        <div class="mt10 moment-footer p-flex-s-right">
+                            <div class="col-12 col-md-11">
+                                <div class="p-block moment-content-box">
+                                    <span class="al"></span>
+                                    <div class="mt10 moment-content <?php get_entry_content_class() ?>">
+                                        <?php the_content(); ?>
+                                    </div>
+                                    <div class="mt10 moment-footer p-flex-s-right">
                             <span class="t-sm c-sub">
+                            <span class="mr-2"><i class="fa-regular fa-clock mr-1"></i><?php pk_get_post_date(); ?></span>
                             <?php if (!pk_is_checked('hide_post_views')): ?>
                                 <span class="mr-2"><i
                                             class="fa-regular fa-eye mr-1"></i><?php pk_get_post_views() ?></span>
@@ -55,7 +59,10 @@ get_header();
                                 <i class="fa-regular fa-comment mr-1"></i>
                                 <?php echo get_comments_number() ?></a>
                                 <?php endif; ?>
-                    </span>
+                                </span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 <?php endforeach;
