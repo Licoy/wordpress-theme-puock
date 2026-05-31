db2c4498d44cbab8f2133733a247f2fe0204c4fe
憧憬Licoy <licoycn@gmail.com>
2026-06-01 00:31:48 +0800

    Make cms four-grid responsive

--- templates/module-four-grid.php
+++ templates/module-four-grid.php
@@ -4,9 +4,13 @@ global $cms_four_grid_ava;
 $cms_four_grid_items = pk_is_checked('cms_show_four_grid')
     ? (is_array($cms_four_grid_ava) ? $cms_four_grid_ava : pk_ava_cms_four_grid_items())
     : false;
-if (is_array($cms_four_grid_items) && count($cms_four_grid_items) > 0):
+$cms_four_grid_count = is_array($cms_four_grid_items) ? count($cms_four_grid_items) : 0;
+if ($cms_four_grid_count > 0):
+    $cms_four_grid_columns = max(1, min(4, $cms_four_grid_count));
+    $cms_four_grid_mobile_columns = max(1, min(2, $cms_four_grid_count));
     ?>
-    <div id="cms-four-grid" class="row g-2 g-md-3 mb15">
+    <div id="cms-four-grid" class="mb15"
+         style="<?php echo esc_attr(sprintf('--cms-four-grid-columns:%d;--cms-four-grid-mobile-columns:%d;', $cms_four_grid_columns, $cms_four_grid_mobile_columns)); ?>">
         <?php foreach ($cms_four_grid_items as $item):
             $title = $item['title'] ?? '';
             $img = $item['img'] ?? '';
@@ -14,19 +18,17 @@ if (is_array($cms_four_grid_items) && count($cms_four_grid_items) > 0):
             $open_blank = filter_var($item['blank'] ?? false, FILTER_VALIDATE_BOOLEAN);
             $content = '<img alt="' . esc_attr($title) . '" ' . pk_get_lazy_img_info($img, 'cms-four-grid-img', 320, 128) . ' />';
             ?>
-            <div class="col-6 col-md-3">
-                <?php if (!empty($link)): ?>
-                    <a data-no-instant class="cms-four-grid-item cms-four-grid-link"
-                       href="<?php echo esc_url($link); ?>"
-                        <?php echo $open_blank ? ' target="_blank" rel="noopener noreferrer"' : ''; ?>>
-                        <?php echo $content; ?>
-                    </a>
-                <?php else: ?>
-                    <div class="cms-four-grid-item">
-                        <?php echo $content; ?>
-                    </div>
-                <?php endif; ?>
-            </div>
+            <?php if (!empty($link)): ?>
+                <a data-no-instant class="cms-four-grid-item cms-four-grid-link"
+                   href="<?php echo esc_url($link); ?>"
+                    <?php echo $open_blank ? ' target="_blank" rel="noopener noreferrer"' : ''; ?>>
+                    <?php echo $content; ?>
+                </a>
+            <?php else: ?>
+                <div class="cms-four-grid-item">
+                    <?php echo $content; ?>
+                </div>
+            <?php endif; ?>
         <?php endforeach; ?>
     </div>
 <?php endif; ?>
